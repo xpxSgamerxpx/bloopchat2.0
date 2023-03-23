13b912328385373855f7307759bae93c77338cb2
shlok <1bataviashl@gmail.com>
2023-03-23 18:53:47 -0400

    first commit

--- pretty-chat-react-typescript-master/src/AuthPage/LogInForm.tsx
+++ pretty-chat-react-typescript-master/src/AuthPage/LogInForm.tsx
@@ -64,7 +64,7 @@ const LogInForm = (props: LogInFormProps) => {
         <TextInput
           label="Email"
           name="email"
-          placeholder="adam@lamorre.co"
+          placeholder="1bataviashl@gmail.com"
           onChange={(e) => setEmail(e.target.value)}
         />
 
